1235210a0aae7f4cb5bbb57c84ab6a019b82df12
sonuvandan123 <vandanmishra6869@gmail.com>
2020-06-08 23:39:00 -0700

    first commit 9/06/2020

---
+++ src/test/java/fourth/fourth/AddPlace.java
@@ -0,0 +1,28 @@
+package fourth.fourth;
+
+/* below package is use for given() method need to manually 
+add this because of static packages not suggested by*/
+import static io.restassured.RestAssured.given;
+/*below package is used to add assertion in body. Again need to add manually.
+body("scope",equalTo("APP")
+*/
+import static org.hamcrest.Matchers.*;
+import io.restassured.RestAssured;
+public class AddPlace 
+{
+	public static void main(String[] args) {
+	RestAssured.baseURI="https://rahulshettyacademy.com";
+		
+		// given all input details
+		// when submit the API
+		// then validate the response
+		// resourse and method will always go to when()
+		// we can validate in body as well using 
+		given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+		.body(PayLoad.jsonPayload())
+		.when().post("maps/api/place/add/json")
+		.then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP"))
+		.header("Server","Apache/2.4.18 (Ubuntu)");
+		;
+	}
+}
---
+++ src/test/java/fourth/fourth/PayLoad.java
@@ -0,0 +1,25 @@
+package fourth.fourth;
+
+public class PayLoad 
+{
+	public static String jsonPayload()
+	{
+		return "{\r\n" + 
+				"  \"location\": {\r\n" + 
+				"    \"lat\": -38.383494,\r\n" + 
+				"    \"lng\": 33.427362\r\n" + 
+				"  },\r\n" + 
+				"  \"accuracy\": 50,\r\n" + 
+				"  \"name\": \"Satya Book Store\",\r\n" + 
+				"  \"phone_number\": \"8913567825\",\r\n" + 
+				"  \"address\": \"Haideraband\",\r\n" + 
+				"  \"types\": [\r\n" + 
+				"    \"Gali no 4\",\r\n" + 
+				"    \"Chulakana city\"\r\n" + 
+				"  ],\r\n" + 
+				"  \"website\": \"hbchyderabad.com\",\r\n" + 
+				"  \"language\": \"English\"\r\n" + 
+				"}";
+		
+	}
+}
